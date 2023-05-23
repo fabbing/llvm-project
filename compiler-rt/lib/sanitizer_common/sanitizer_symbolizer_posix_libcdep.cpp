99353ef796d1f51cf1cac78a941a9ce6072d45d1
fabbing <fabrice@tarides.com>
2023-05-23 12:10:22 +0200

    Honor external symbolizer flag

--- compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_posix_libcdep.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_posix_libcdep.cpp
@@ -470,6 +470,11 @@ static void ChooseSymbolizerTools(IntrusiveList<SymbolizerTool> *list,
     VReport(2, "Symbolizer is disabled.\n");
     return;
   }
+
+  if (SymbolizerTool *tool = ChooseExternalSymbolizer(allocator)) {
+    list->push_back(tool);
+  }
+
   if (IsAllocatorOutOfMemory()) {
     VReport(2, "Cannot use internal symbolizer: out of memory\n");
   } else if (SymbolizerTool *tool = InternalSymbolizer::get(allocator)) {
@@ -481,10 +486,6 @@ static void ChooseSymbolizerTools(IntrusiveList<SymbolizerTool> *list,
     VReport(2, "Using libbacktrace symbolizer.\n");
     list->push_back(tool);
     return;
-  }
-
-  if (SymbolizerTool *tool = ChooseExternalSymbolizer(allocator)) {
-    list->push_back(tool);
   }
 
 #if SANITIZER_APPLE
